fa782951565b37ee162c4004b11ad1dc2d8d4d37
Manish Pandey <119288527+manishpandeyprojects@users.noreply.github.com>
2022-12-14 13:25:59 +0530

    MP: Create sameOutput Json files & created the cards for news using bootstrap.

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,19 @@ import NewsItems from './NewsItems'
 export default class News extends Component {
   render() {
     return (
-      <div>
-        This is the news compon
-        <NewsItems />
+      <div className='container my-3'>
+        <h3>NewsMonkey - Top Headlines</h3>
+        <div className="row">
+            <div className="col-md-4">
+              <NewsItems title="MyTitle" description="MyDescription" />
+            </div>
+            <div className="col-md-4">
+              <NewsItems title="MyTitle" description="MyDescription" />
+            </div>
+            <div className="col-md-4">
+              <NewsItems title="MyTitle" description="MyDescription" />
+            </div>
+        </div>
       </div>
     )
   }
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react'
 
 export default class NewsItems extends Component {
   render() {
+    let {title,description} = this.props;
     return (
-      <div>
-        This is the newItems components
+      <div className="card" style={{width: "100%"}}>
+        <img src="https://cdn.24.co.za/files/Cms/General/d/10743/97d776dc91734e98906c0e1b7f3b1afa.jpg" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="/newsDetails" className="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
     )
   }
