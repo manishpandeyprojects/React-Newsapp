0d5b7e58ea96a68541703befcd4f0abd993ade73
Manish Pandey <119288527+manishpandeyprojects@users.noreply.github.com>
2022-12-14 12:45:43 +0530

    MP: Added the bootstrap and created the basic class based components.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>This is my first react class based components</h1>
+        <Navbar />
+        <News />
       </div>
     )
   }
---
+++ src/components/NewsItems.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NewsItems extends Component {
+  render() {
+    return (
+      <div>
+        This is the newItems components
+      </div>
+    )
+  }
+}
