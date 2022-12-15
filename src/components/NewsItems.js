6dfd877a7c77b42809cb0c0469cbbf3c3e52b291
Manish Pandey <119288527+manishpandeyprojects@users.noreply.github.com>
2022-12-15 21:22:52 +0530

    MP: Bind the news items getting the state from the local json

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 
 export default class NewsItems extends Component {
   render() {
-    let {title,description} = this.props;
+    let {title,description, imageUrl, newsUrl} = this.props;
     return (
       <div className="card" style={{width: "100%"}}>
-        <img src="https://cdn.24.co.za/files/Cms/General/d/10743/97d776dc91734e98906c0e1b7f3b1afa.jpg" className="card-img-top" alt="..." />
+        <img src={imageUrl} className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{description}</p>
-          <a href="/newsDetails" className="btn btn-primary">Go somewhere</a>
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
         </div>
       </div>
     )
