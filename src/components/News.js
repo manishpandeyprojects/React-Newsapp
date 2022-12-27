8f4c7cd11c6062867250ea8d814d5ad4953e040a
Manish Pandey <119288527+manishpandeyprojects@users.noreply.github.com>
2022-12-27 21:29:42 +0530

    MP: Fixed the infinite scroll loader issues

--- src/components/News.js
+++ src/components/News.js
@@ -5,11 +5,10 @@ import Spinner from './Spinner';
 import InfiniteScroll from "react-infinite-scroll-component";
 
 
-
 export default class News extends Component {
   constructor(props) {
     super(props);
-    this.pageSize = 12;
+    this.pageSize = 6;
     this.state = {
       article: [],
       loading: false,
@@ -26,7 +25,7 @@ export default class News extends Component {
   };
 
   static defaultProps = {
-    category: "genral",
+    category: "business",
     country: "in",
   };
 
@@ -36,55 +35,54 @@ export default class News extends Component {
 
   async fetchData() {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ecf49a858023457ca2baee9120a09dbb&pageSize=${this.pageSize}&page=${this.state.page}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parseData = await data.json();
-    this.setState(
-      {
-        article: this.state.article.concat(parseData.articles),
-        totalPage: Math.ceil(parseData.totalResults / this.pageSize),
-        totalResults: parseData.totalResults,
-        loading: false
-      }
-    );
+    this.setState({ article: parseData.articles, totalPage: Math.ceil(parseData.totalResults / this.pageSize), totalResults: parseData.totalResults, loading: false, page: this.state.page + 1 });
   }
 
   async componentDidMount() {
     this.fetchData();
-    this.setState({ loading: true });
   }
 
-
   fetchMoreData = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ecf49a858023457ca2baee9120a09dbb&pageSize=${this.pageSize}&page=${this.state.page}`;
+    console.log(url)
+    let data = await fetch(url);
+    let parseData = await data.json();
+    console.log(parseData.articles)
+    this.setState({ article: this.state.article.concat(parseData.articles) });
     this.setState({ page: this.state.page + 1 });
-    this.fetchData();
   };
+
 
   render() {
     return (
       <>
-        <div className=' my-3'>
-          <div className="container">
-            <h1 className='text-center' style={{ margin: "40px 0" }}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
-            {this.state.loading && <Spinner className="d-flex justify-content-center spinner" />}
-          </div>
-
-          <InfiniteScroll
-            dataLength={this.state.article.length}
-            next={this.fetchMoreData}
-            hasMore={this.state.article.length !== this.state.totalResults}
-            loader={<Spinner className="d-flex justify-content-center my-3" />} 
-          >
-            <div className='container my-3'>
-              <div className="row" style={this.state.loading ? { opacity: '0.5' } : {}}>
-                {this.state.article?.map((element, index) => {
-                  return <div className="col-md-4 my-2" key={index}>
-                    <NewsItems title={element.title ? element.title.slice(0, 72) : ""} description={element.description ? element.description.slice(0, 148) : ""} imageUrl={element.urlToImage ? element.urlToImage : "https://cdn.zeebiz.com/sites/default/files/2022/12/20/217009-m4-freepik.jpg"} newsUrl={element.url ? element.url : ""} author={element.author ? element.author : "Unknown"} date={element.publishedAt} source={element.source.name} />
-                  </div>
-                })}
-              </div>
-            </div>
-          </InfiniteScroll>
+        <div className='container my-3'>
+          <h1 className='text-center' style={{ margin: "40px 0" }}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
+          {this.state.loading && <Spinner className="spinner text-center my-3" />}
         </div>
+
+        {console.log("totalResults " + this.state.totalResults)}
+        {console.log("article " + this.state.article.length)}
+        <InfiniteScroll
+          dataLength={this.state.article.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.article.length !== this.state.totalResults}
+          loader={<Spinner className="text-center my-3" />}
+        >
+          <div className='container'>
+        
+            <div className="row" style={this.state.loading ? { opacity: '0.5' } : {}}>
+              {this.state.article?.map((element, index) => {
+                return <div className="col-md-4 my-2" key={index}>
+                  <NewsItems key={element.url} title={element.title ? element.title.slice(0, 72) : ""} description={element.description ? element.description.slice(0, 148) : ""} imageUrl={element.urlToImage ? element.urlToImage : "https://cdn.zeebiz.com/sites/default/files/2022/12/20/217009-m4-freepik.jpg"} newsUrl={element.url ? element.url : ""} author={element.author ? element.author : "Unknown"} date={element.publishedAt} source={element.source.name} />
+                </div>
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
       </>
     )
   }
